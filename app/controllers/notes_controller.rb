8fece2eef512ad0bc0c14ff9fcdbbea685e844d0
Berry  Semexan <bsemexan@gmail.com>
2018-04-13 01:57:14 +0000

    Done.

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,20 @@
+class NotesController < ApplicationController
+
+
+  def create
+    note = Note.new(note_params)
+    note.user = current_user
+    note.save!
+    redirect_to '/'
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    @note.update(note_params)
+    redirect_to '/'
+  end
+
+  def note_params
+    params.require(:note).permit(:content, :visible_to)
+  end
+end
